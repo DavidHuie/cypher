85383098ff1106246936feea81abe5b557c4ee68
David Huie <dahuie@gmail.com>
2013-12-10 17:40:09 +0000

    Fixed broken server spec

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -5,12 +5,12 @@ describe Cypher::Server do
   let(:klass) { Cypher::Server }
   let(:uri) { 'druby://localhost:1234' }
   let(:server) { klass.new(uri, 2) }
-  let(:file) { Tempfile.new('new_test') }
+  let(:file_path) { '/tmp/test_file' }
 
   describe '#authenticate' do
 
     it 'returns false if a repo does not exist or if a password is wrong' do
-      server.authenticate(file.path, 'password').
+      server.authenticate(file_path, 'password').
         should be_false
     end
 
